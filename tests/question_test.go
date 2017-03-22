7d6f9f1ad113c1637f95823a69c5f0909b768e50
Mahdi Bolow <boolow5@gmail.com>
2017-03-23 01:14:31 +0300

    added unittests for questions controllers

--- tests/question_test.go
+++ tests/question_test.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -18,10 +17,10 @@ func TestAddQuestion(t *testing.T) {
 	t.Log("TestAddQuestion")
 	gin.SetMode(gin.TestMode)
 	r := gin.Default()
-	r.POST("/question", controllers.AddUser)
+	r.POST("/question", controllers.AddQuestion)
 
 	// create request
-	body := strings.NewReader(`{"text":"Wax iweydi sxb?", "author":{"user_id":"1"}}`)
+	body := strings.NewReader(`{"text":"Wax iweydi sxb?", "author":{"user_id":1}}`)
 	req, err := http.NewRequest(http.MethodPost, "/question", body)
 	if err != nil {
 		t.Fatalf("Could not create request: %v\n", err)
@@ -38,9 +37,11 @@ func TestAddQuestion(t *testing.T) {
 	var responseJson map[string]interface{}
 	json.Unmarshal(rec.Body.Bytes(), &responseJson)
 	// check response body
-	expected_data := `{"success": "saved successfully"}`
-	if fmt.Sprintf("%v", responseJson) == expected_data {
-		t.Fatalf("Expected to get value %v but instead got %v ", expected_data, responseJson)
+	expected_data := "saved successfully"
+	if responseJson["success"] != nil {
+		if strings.ToLower(responseJson["success"].(string)) != strings.ToLower(expected_data) {
+			t.Fatalf("Expected to get value %v but instead got %v ", expected_data, responseJson["success"])
+		}
 	}
 }
 
@@ -49,11 +50,11 @@ func TestUpdateQuestion(t *testing.T) {
 	t.Log("TestUpdateQuestion")
 	gin.SetMode(gin.TestMode)
 	r := gin.Default()
-	r.PUT("/question", controllers.AddUser)
+	r.PUT("/question", controllers.UpdateQuestion)
 
 	// create request
 	body := strings.NewReader(`{"question_id":1,"text":"Su'aasha si kale makuu fahansiiyaa?"}`)
-	req, err := http.NewRequest(http.MethodPut, "/question", body)
+	req, err := http.NewRequest(http.MethodPut, "/question?question_id=1", body)
 	if err != nil {
 		t.Fatalf("Could not create request: %v\n", err)
 	}
@@ -69,9 +70,11 @@ func TestUpdateQuestion(t *testing.T) {
 	var responseJson map[string]interface{}
 	json.Unmarshal(rec.Body.Bytes(), &responseJson)
 	// check response body
-	expected_data := `{"success": "updated successfully"}`
-	if fmt.Sprintf("%v", responseJson) == expected_data {
-		t.Fatalf("Expected to get value %v but instead got %v ", expected_data, responseJson)
+	expected_data := "updated successfully"
+	if responseJson["success"] != nil {
+		if strings.ToLower(responseJson["success"].(string)) != strings.ToLower(expected_data) {
+			t.Fatalf("Expected to get value \"%v\" but instead got \"%v\" ", expected_data, responseJson["success"])
+		}
 	}
 }
 
@@ -80,13 +83,13 @@ func TestDeleteQuestion(t *testing.T) {
 	t.Log("TestDeleteQuestion")
 	gin.SetMode(gin.TestMode)
 	r := gin.Default()
-	r.DELETE("/question", controllers.AddUser)
+	r.DELETE("/question", controllers.DeleteQuestion)
 
 	// create request
 	body := strings.NewReader(`{"question_id":1}`)
-	req, err := http.NewRequest(http.MethodDelete, "/question", body)
+	req, err := http.NewRequest(http.MethodDelete, "/question?question_id=1", body)
 	if err != nil {
-		t.Fatalf("Could not create request: %v\n", err)
+		t.Fatalf("Could not create request: \"%v\"\n", err)
 	}
 	// create response recorder
 	rec := httptest.NewRecorder()
@@ -94,14 +97,17 @@ func TestDeleteQuestion(t *testing.T) {
 	r.ServeHTTP(rec, req)
 	// check status
 	if rec.Code != http.StatusOK {
-		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, rec.Code)
+		t.Fatalf("Expected to get status \"%d\" but instead got \"%d\"\n", http.StatusOK, rec.Code)
 	}
 	// Unmarshal the response body
 	var responseJson map[string]interface{}
 	json.Unmarshal(rec.Body.Bytes(), &responseJson)
 	// check response body
-	expected_data := `{"success": "saved successfully"}`
-	if fmt.Sprintf("%v", responseJson) == expected_data {
-		t.Fatalf("Expected to get value %v but instead got %v ", expected_data, responseJson)
+	expected_data := "deleted successfully"
+	if responseJson["error"] != nil {
+		t.Fatalf("Expected to get value \"%v\" but instead got \"%v\" ", expected_data, responseJson["error"])
+	}
+	if strings.ToLower(responseJson["success"].(string)) != strings.ToLower(expected_data) {
+		t.Fatalf("Expected to get value \"%v\" but instead got \"%v\" ", expected_data, responseJson["success"])
 	}
 }
