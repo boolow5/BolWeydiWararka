6672d166e4725f124ec66b3aa32dde9506e2fe79
Mahdi Bolow <boolow5@gmail.com>
2017-03-23 01:09:31 +0300

    moved endpoints to a secure group route

--- main.go
+++ main.go
@@ -21,12 +21,16 @@ func main() {
 		auth.GET("/hello", func(c *gin.Context) {
 			c.JSON(200, gin.H{"message": "Welcome you're authorized to see this!"})
 		})
+
+		auth.PUT("/user", controllers.UpdateUser)
+		auth.DELETE("/user", controllers.DeleteUser)
+		//auth.POST("/auth", controllers.AuthenticateUser)
+
+		auth.POST("/question", controllers.AddQuestion)
+		auth.PUT("/question", controllers.UpdateQuestion)
+		auth.DELETE("/question", controllers.DeleteQuestion)
 	}
 	router.POST("/user", controllers.AddUser)
-	router.PUT("/user", controllers.UpdateUser)
-	router.DELETE("/user", controllers.DeleteUser)
-	//router.POST("/auth", controllers.AuthenticateUser)
-
 	router.POST("/login", jwtMiddleware.LoginHandler)
 
 	port := os.Getenv("PORT")
