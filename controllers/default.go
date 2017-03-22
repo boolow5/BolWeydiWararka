8b154d5cabb6388a02c24a5c781042b6ad3770c3
Mahdi Bolow <boolow5@gmail.com>
2017-03-23 01:10:51 +0300

    from hardcoded user_id to query string version

--- controllers/default.go
+++ controllers/default.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/boolow5/BolWeydi/models"
 
 	"gopkg.in/gin-gonic/gin.v1"
@@ -42,8 +44,22 @@ func AddUser(context *gin.Context) {
 }
 
 func UpdateUser(context *gin.Context) {
-	_, user := models.GetUserById(1)
-	err := context.BindJSON(&user)
+	id, got := context.GetQuery("user_id")
+	if !got {
+		context.JSON(200, gin.H{"error": "missing user id"})
+		return
+	}
+	userId, err := strconv.Atoi(id)
+	if err != nil {
+		context.JSON(200, gin.H{"error": err.Error()})
+		return
+	}
+	if userId < 1 {
+		context.JSON(200, gin.H{"error": "invalid user id"})
+		return
+	}
+	_, user := models.GetUserById(userId)
+	err = context.BindJSON(&user)
 	if err != nil {
 		context.JSON(200, gin.H{"error": err.Error()})
 		return
@@ -61,7 +77,21 @@ func UpdateUser(context *gin.Context) {
 }
 
 func DeleteUser(context *gin.Context) {
-	_, user := models.GetUserById(1)
+	id, got := context.GetQuery("user_id")
+	if !got {
+		context.JSON(200, gin.H{"error": "missing user id"})
+		return
+	}
+	userId, err := strconv.Atoi(id)
+	if err != nil {
+		context.JSON(200, gin.H{"error": err.Error()})
+		return
+	}
+	if userId < 1 {
+		context.JSON(200, gin.H{"error": "invalid user id"})
+		return
+	}
+	_, user := models.GetUserById(userId)
 	deleted, err := user.Delete()
 	data := map[string]interface{}{}
 	if err != nil {
